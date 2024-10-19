6d62ab14794c7603c2d5f3622db471f7557d60da
Yogita Dongare <yogitadongare06@gmail.com>
2024-10-19 14:12:52 +0530

    Added author model file

---
+++ Libray_Management_Project/Libray_Management_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Libray_Management_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
